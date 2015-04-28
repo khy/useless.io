e17227cbdeaafb06cc6aaca9c8cd8ccf4dce09a8
Kevin Hyland <khy@me.com>
2015-04-27 22:32:12 -0400

    More updates to confs

--- apps/account/app/helpers/UrlHelper.scala
+++ apps/account/app/helpers/UrlHelper.scala
@@ -6,7 +6,7 @@ object UrlHelper {
 
   def authUrl = {
     val config = Play.current.configuration
-    val authBaseUrl = config.getString("account.baseUrl").get
+    val authBaseUrl = config.getString("account.authBaseUrl").get
     val appGuid = config.getString("account.appGuid").get
     val scopes = "admin"
     authBaseUrl + s"/auth?app_guid=$appGuid&scopes=$scopes"
