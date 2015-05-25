17cdce9d5c8c72b042215639b95aa324cda24066
Kevin Hyland <khy@me.com>
2015-05-24 22:28:16 -0400

    Ensure that boot2docker is running before publishing to Docker

--- project/Release.scala
+++ project/Release.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys.publish
 
 import sbtrelease.ReleasePlugin
+import ReleasePlugin.autoImport._
 import sbtrelease.ReleaseStateTransformations._
 import com.typesafe.sbt.packager.docker.DockerPlugin
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport.Docker
@@ -10,16 +11,31 @@ object Release extends AutoPlugin {
 
   override def requires = DockerPlugin && ReleasePlugin
 
+  object autoImport {
+
+    val ensureBoot2Docker = taskKey[Unit](
+      "Ensure that boot2docker is running."
+    )
+
+  }
+
+  import autoImport._
+
   override def projectSettings = Seq(
 
-    ReleasePlugin.autoImport.releaseProcess := Seq(
+    ensureBoot2Docker := {
+      "boot2docker up".!
+    },
+
+    releaseProcess := Seq(
       checkSnapshotDependencies,
       inquireVersions,
       runTest,
       setReleaseVersion,
       commitReleaseVersion,
       tagRelease,
-      ReleasePlugin.autoImport.releaseStepTask(publish in Docker),
+      releaseStepTask(ensureBoot2Docker),
+      releaseStepTask(publish in Docker),
       setNextVersion,
       commitNextVersion,
       pushChanges
