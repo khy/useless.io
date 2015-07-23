a12bf19e63d18eaa98ea61a830260d4e0b72d485
Kevin Hyland <khy@me.com>
2015-07-22 21:09:20 -0400

    Move haiku pagination spec to ScalaTest

--- modules/apis/haiku/test/lib/PaginationSpec.scala
+++ modules/apis/haiku/test/lib/PaginationSpec.scala
@@ -1,50 +1,50 @@
 package lib.haiku
 
 import java.util.UUID
-import org.specs2.mutable._
+import org.scalatestplus.play.PlaySpec
 import play.api.test._
 import play.api.test.Helpers._
 
-class PaginationSpec extends Specification {
+class PaginationSpec extends PlaySpec {
 
-  "Pagination#count" should {
+  "Pagination#count" must {
 
     "return the count query paramter as an optional Int, if specified" in {
       val request = FakeRequest(GET, "/path?count=20")
-      Pagination(request).count must beEqualTo (Some(20))
+      Pagination(request).count mustBe Some(20)
     }
 
     "return None if count is not specified as a query parameter" in {
       val request = FakeRequest(GET, "/path")
-      Pagination(request).count must beEqualTo (None)
+      Pagination(request).count mustBe None
     }
 
   }
 
-  "Pagination#since" should {
+  "Pagination#since" must {
 
     "return the since query parameter as an optional UUID, if specified" in {
       val request = FakeRequest(GET, "/path?since=00000000-0000-0000-0000-000000000000")
-      Pagination(request).since must beEqualTo (Some(UUID.fromString("00000000-0000-0000-0000-000000000000")))
+      Pagination(request).since mustBe Some(UUID.fromString("00000000-0000-0000-0000-000000000000"))
     }
 
     "return None if since is not specified as a query parameter" in {
       val request = FakeRequest(GET, "/path")
-      Pagination(request).since must beEqualTo (None)
+      Pagination(request).since mustBe None
     }
 
   }
 
-  "Pagination#until" should {
+  "Pagination#until" must {
 
     "return the until query parameter as an optional UUID, if specified" in {
       val request = FakeRequest(GET, "/path?until=00000000-0000-0000-0000-000000000000")
-      Pagination(request).until must beEqualTo (Some(UUID.fromString("00000000-0000-0000-0000-000000000000")))
+      Pagination(request).until mustBe Some(UUID.fromString("00000000-0000-0000-0000-000000000000"))
     }
 
     "return None if until is not specified as a query parameter" in {
       val request = FakeRequest(GET, "/path")
-      Pagination(request).until must beEqualTo (None)
+      Pagination(request).until mustBe None
     }
 
   }
