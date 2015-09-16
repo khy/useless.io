a6b0e756761654bde763974947da25048abc8cfe
Kevin Hyland <khy@me.com>
2015-09-15 23:09:09 -0400

    budget: Don't use 'external' AccountType

--- modules/apis/budget/app/models/AccountType.scala
+++ modules/apis/budget/app/models/AccountType.scala
@@ -8,9 +8,8 @@ object AccountType extends KeyedResolver[AccountType] {
   case object Credit extends AccountType { val key = "credit" }
   case object Checking extends AccountType { val key = "checking" }
   case object Savings extends AccountType { val key = "savings" }
-  case object External extends AccountType { val key = "external" }
   case class Unknown(key: String) extends AccountType
 
-  val values = Seq(Credit, Checking, Savings, External)
+  val values = Seq(Credit, Checking, Savings)
   def unknown(key: String) = Unknown(key)
 }
