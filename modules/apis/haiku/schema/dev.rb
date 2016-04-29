c937d5256d744af0048ec4025ea45b57f0d202f9
Kevin Hyland <khy@me.com>
2016-04-29 17:41:58 -0400

    Add schema-evolution-manager wrapper scripts

---
+++ modules/apis/haiku/schema/dev.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# == Wrapper script to update a local postgrseql database
+#
+# == Usage
+#  ./dev.rb
+#
+
+Dir.chdir(File.dirname($0)) {
+  command = "sem-apply --url postgres://localhost:5432/useless_haiku_dev"
+  puts command
+  exec(command)
+}
