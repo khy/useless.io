845e9a4e790940ca92e1a0762518c8e3836c3283
Kevin Hyland <khy@me.com>
2016-04-29 18:01:16 -0400

    Add first SQL script for haiku

---
+++ modules/apis/haiku/schema/scripts/20160429-175815.sql
@@ -0,0 +1,19 @@
+CREATE TABLE haikus (
+    id bigserial NOT NULL PRIMARY KEY,
+    uuid uuid NOT NULL,
+    line_one text NOT NULL,
+    line_two text NOT NULL,
+    line_three text NOT NULL,
+    in_response_to_id bigint NOT NULL REFERENCES haikus,
+    attribution text,
+    created_at timestamp NOT NULL DEFAULT now(),
+    created_by_account uuid NOT NULL,
+    created_by_access_token uuid NOT NULL,
+    deleted_at timestamp,
+    deleted_by_account uuid,
+    deleted_by_access_token uuid
+);
+
+CREATE INDEX haikus_in_response_to_id_idx ON haikus (in_response_to_id);
+CREATE INDEX haikus_created_by_account_idx ON haikus (created_by_account);
+CREATE INDEX haikus_created_at_desc_idx ON haikus (created_at DESC);
