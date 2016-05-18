cfae2db5e5bfc0691875634b157ef20fe4daee3d
Kevin Hyland <khy@me.com>
2016-05-17 21:19:14 -0400

    Add test script to core schema

--- modules/apis/core/schema/test.rb
+++ modules/apis/core/schema/test.rb
@@ -2,11 +2,11 @@
 # == Wrapper script to update a local postgrseql database
 #
 # == Usage
-#  ./dev.rb
+#  ./test.rb
 #
 
 Dir.chdir(File.dirname($0)) {
-  command = "sem-apply --url postgres://localhost/useless_core_dev"
+  command = "sem-apply --url postgres://localhost/useless_core_test"
   puts command
   exec(command)
 }
