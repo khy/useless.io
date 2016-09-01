23eaa6a710e3467000a41b692123982174491eed
Kevin Hyland <kevin@evenfinancial.com>
2016-08-31 21:47:45 -0400

    Drastically simplify AppHelper in books tests

--- modules/apis/books/test/util/AppHelper.scala
+++ modules/apis/books/test/util/AppHelper.scala
@@ -1,38 +1,21 @@
 package test.util
 
 import java.util.UUID
-import play.api.BuiltInComponents
 import play.api.test.Helpers._
-import play.api.db.slick.SlickComponents
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import io.useless.client.account.AccountClientComponents
 import io.useless.accesstoken.AccessToken
 
 import init.books.AbstractApplicationComponents
-import clients.books.ClientComponents
-import services.books.ServiceComponents
-import services.books.db.DbConfigComponents
 import services.books.db._
 
 class AppHelper(
-  applicationComponents: AbstractApplicationComponents with ClientComponents with AccountClientComponents
-)
-  extends SlickComponents
-  with AccountClientComponents
-  with DbConfigComponents
-  with ClientComponents
-  with ServiceComponents
-{
+  applicationComponents: AbstractApplicationComponents
+){
+
+  import applicationComponents._
 
   import dbConfig.db
   import dbConfig.driver.api._
-
-  def applicationLifecycle = applicationComponents.applicationLifecycle
-  def configuration = applicationComponents.configuration
-  def environment = applicationComponents.environment
-
-  def accountClient = applicationComponents.accountClient
-  def editionClient = applicationComponents.editionClient
 
   def clearNotes() {
     db.run(sqlu"delete from notes")
