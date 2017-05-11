429df13f5377e44e3a8f82aa8e1c9c12f83fb0cb
Kevin Hyland <kevin@evenfinancial.com>
2017-05-10 23:34:10 -0400

    Add constraint validation to WorkoutValidator

--- modules/apis/workouts/app/dsl/WorkoutValidator.scala
+++ modules/apis/workouts/app/dsl/WorkoutValidator.scala
@@ -3,16 +3,36 @@ package dsl.workouts
 import play.api.libs.json.JsPath
 import play.api.data.validation.ValidationError
 
-import models.workouts.core._
+import models.workouts._
 
 object WorkoutValidator {
 
-  def validateWorkout(workout: Workout): Seq[(JsPath, Seq[ValidationError])] = {
-    appendJsPath(JsPath \ "task", validateTask(workout.task))
+  def validateWorkout(workout: core.Workout, referencedMovements: Seq[Movement]): Seq[(JsPath, Seq[ValidationError])] = {
+    appendJsPath(JsPath \ "task", validateTask(workout.task, referencedMovements))
   }
 
-  private def validateTask(task: AbstractTask): Seq[(JsPath, Seq[ValidationError])] = {
+  private def validateTask(task: core.AbstractTask, referencedMovements: Seq[Movement]): Seq[(JsPath, Seq[ValidationError])] = {
     var errors = Seq.empty[(JsPath, Seq[ValidationError])]
+
+    task.movement.foreach { movement =>
+      val refMovement = referencedMovements.find { _.guid == movement }
+
+      refMovement.foreach { refMovement =>
+        task.constraints.foreach { constraints =>
+          constraints.zipWithIndex.foreach { case (constraint, index) =>
+            val variable = refMovement.variables.getOrElse(Nil).find { _.name == constraint.variable }
+
+            if (variable.isEmpty) {
+              errors = errors :+ ((JsPath \ "constraints")(index) \ "variable", Seq(ValidationError("unknown")))
+            }
+          }
+        }
+      }
+
+      if (refMovement.isEmpty) {
+        errors = errors :+ (JsPath \ "movement", Seq(ValidationError("unknown")))
+      }
+    }
 
     if (task.movement.isDefined && task.tasks.isDefined) {
       errors = errors :+ (JsPath, Seq(ValidationError("movement-and-tasks-specified")))
@@ -24,7 +44,7 @@ object WorkoutValidator {
 
     task.tasks.foreach { tasks =>
       errors = errors ++ tasks.zipWithIndex.flatMap { case (task, index) =>
-        appendJsPath((JsPath \ "tasks")(index), validateTask(task))
+        appendJsPath((JsPath \ "tasks")(index), validateTask(task, referencedMovements))
       }
     }
 
--- modules/apis/workouts/test/dsl/WorkoutValidatorSpec.scala
+++ modules/apis/workouts/test/dsl/WorkoutValidatorSpec.scala
@@ -1,30 +1,39 @@
 package dsl.workouts
 
 import java.util.UUID
+import play.api.test.Helpers._
 import play.api.libs.json.JsPath
 
+import db.workouts.MovementRecord
+import models.workouts._
 import test.workouts.IntegrationSpec
 
 class WorkoutValidatorSpec extends IntegrationSpec {
 
   import testHelper._
 
+  def db2api(record: MovementRecord): Movement = {
+    await { applicationComponents.movementsService.db2api(Seq(record)) }.head
+  }
+
   "WorkoutValidator.validateWorkout" must {
 
     "accept the workout built by TestHelper" in {
       val workout = buildWorkout()
-      WorkoutValidator.validateWorkout(workout) mustBe Nil
+      WorkoutValidator.validateWorkout(workout, Nil) mustBe Nil
     }
 
     "reject a workout with a task with both a movement and tasks" in {
+      val movement = db2api(createMovement())
+
       val workout = buildWorkout(
         task = buildAbstractTask(
-          movement = Some(UUID.randomUUID),
+          movement = Some(movement.guid),
           tasks = Some(Seq(buildAbstractTask()))
         )
       )
 
-      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout).head
+      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout, Seq(movement)).head
       jsPath mustBe JsPath \ "task"
       errors.head.message mustBe "movement-and-tasks-specified"
     }
@@ -37,24 +46,61 @@ class WorkoutValidatorSpec extends IntegrationSpec {
         )
       )
 
-      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout).head
+      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout, Nil).head
       jsPath mustBe JsPath \ "task"
       errors.head.message mustBe "tasks-and-constraints-specified"
     }
 
+    "reject a workout if any movements are not in referencedMovements" in {
+      val workout = buildWorkout(
+        task = buildAbstractTask(
+          movement = Some(UUID.randomUUID)
+        )
+      )
+
+      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout, Nil).head
+      jsPath mustBe JsPath \ "task" \ "movement"
+      errors.head.message mustBe "unknown"
+    }
+
+    "reject a workout with a task whose variables do not match the movements variables" in {
+      val movement = db2api(createMovement(
+        name = "Clean",
+        variables = Some(Seq(
+          core.FreeVariable(name = "Barbell Weight", dimension = core.Dimension.Weight)
+        ))
+      ))
+
+      val workout = buildWorkout(
+        task = buildAbstractTask(
+          movement = Some(movement.guid),
+          constraints = Some(Seq(buildConstraint(
+            variable = "Box Height",
+            value = core.ConstraintExpression.parse("30 in").right.get
+          )))
+        )
+      )
+
+      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout, Seq(movement)).head
+      jsPath mustBe (JsPath \ "task" \ "constraints")(0) \ "variable"
+      errors.head.message mustBe "unknown"
+    }
+
     "reject a workout with invalid sub-tasks" in {
+      val movement = db2api(createMovement())
+
       val workout = buildWorkout(
         task = buildAbstractTask(
           tasks = Some(Seq(
             buildAbstractTask(
-              movement = Some(UUID.randomUUID),
+              movement = Some(movement.guid),
               tasks = Some(Seq(buildAbstractTask()))
             )
           ))
         )
       )
 
-      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout).head
+      val (jsPath, errors) = WorkoutValidator.validateWorkout(workout, Seq(movement)).head
       jsPath mustBe (JsPath \ "task" \ "tasks")(0)
       errors.head.message mustBe "movement-and-tasks-specified"
     }
