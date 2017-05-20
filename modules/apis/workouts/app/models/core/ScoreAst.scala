c604f8888075a3f797b9781fc8eead06ed14757c
Kevin Hyland <kevin@evenfinancial.com>
2017-05-20 00:37:50 -0400

    Work on the ScoreCompiler

--- modules/apis/workouts/app/models/core/ScoreAst.scala
+++ modules/apis/workouts/app/models/core/ScoreAst.scala
@@ -2,10 +2,22 @@ package models.workouts.core
 
 import dsl.workouts.compile.ScoreCompiler
 
-class ScoreAst(raw: String) extends Ast {
+class ScoreAst(
+  raw: String,
+  val expression: ScoreAst.Expression
+) extends Ast {
   val code = raw
 }
 
 object ScoreAst {
+
   implicit val jsonFormat = Ast.jsonFormat(ScoreCompiler)
+
+  sealed trait Expression
+
+  trait Ref extends Expression
+  case class ImplicitRef(property: String) extends Ref
+  case class ObjectRef(ref: Ref, property: String) extends Ref
+  case class ArrayRef(ref: Ref, index: Int) extends Ref
+
 }
