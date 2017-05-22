dc6a0d636584d4cc552d62187286371a5f3d7ad7
Kevin Hyland <kevin@evenfinancial.com>
2017-05-22 07:31:22 -0400

    Complete basic arithmetic logic

--- modules/apis/workouts/app/dsl/compile/ArithmeticCompiler.scala
+++ modules/apis/workouts/app/dsl/compile/ArithmeticCompiler.scala
@@ -17,6 +17,8 @@ object ArithmeticAst {
   case class Number(value: BigDecimal) extends Expression
   case class Add(left: Expression, right: Expression) extends Expression
   case class Subtract(left: Expression, right: Expression) extends Expression
+  case class Multiply(left: Expression, right: Expression) extends Expression
+  case class Divide(left: Expression, right: Expression) extends Expression
 }
 
 object ArithmeticCompiler extends Compiler[ArithmeticAst] {
@@ -30,12 +32,20 @@ object ArithmeticCompiler extends Compiler[ArithmeticAst] {
   case class NUMBER(text: String) extends Token
   case class PLUS() extends Token
   case class MINUS() extends Token
+  case class TIMES() extends Token
+  case class DIVIDED_BY() extends Token
+  case class OPEN_PAREN() extends Token
+  case class CLOSED_PAREN() extends Token
 
   object Lexer extends RegexParsers {
     val number = positioned("""\d+(\.\d*)?""".r ^^ { str => NUMBER(str) })
-    val plus = positioned("""\+""".r ^^ { str => PLUS() })
-    val minus = positioned("""\-""".r ^^ { str => MINUS() })
-    val tokens = phrase(rep1(number | plus | minus))
+    val plus = positioned("+" ^^ { str => PLUS() })
+    val minus = positioned("-" ^^ { str => MINUS() })
+    val times = positioned("*" ^^ { str => TIMES() })
+    val dividedBy = positioned("\\" ^^ { str => DIVIDED_BY() })
+    val openParen = positioned("(" ^^ { str => OPEN_PAREN() })
+    val closedParen = positioned(")" ^^ { str => CLOSED_PAREN() })
+    val tokens = phrase(rep1(number | plus | minus | times | dividedBy | openParen | closedParen))
 
     def lex(code: String): Either[CompileError, Seq[Token]] = {
       parse(tokens, code) match {
@@ -54,21 +64,35 @@ object ArithmeticCompiler extends Compiler[ArithmeticAst] {
       case NUMBER(text) if Try(BigDecimal(text)).isSuccess => Number(BigDecimal(text))
     })
 
-    val plusMinus = (rep(number ~ PLUS() | number ~ MINUS()) ~ number) ^^ { case ops ~ last =>
-      def trans(last: Number, ops: Seq[~[Number, Token]]): Expression = ops match {
+    val expr1: Parser[Expression] = number | OPEN_PAREN() ~> expr3 <~ CLOSED_PAREN()
+
+    val expr2: Parser[Expression] = (rep(expr1 ~ TIMES() | expr1 ~ DIVIDED_BY()) ~ expr1) ^^ { case ops ~ last =>
+      def tree(last: Expression, ops: Seq[~[Expression, Token]]): Expression = ops match {
         case op :: rest => op match {
-          case number ~ PLUS() => Add(trans(number, rest), last)
-          case number ~ MINUS() => Subtract(trans(number, rest), last)
+          case expr ~ TIMES() => Multiply(tree(expr, rest), last)
+          case expr ~ DIVIDED_BY() => Divide(tree(expr, rest), last)
         }
         case _ => last
       }
 
-      trans(last, ops.reverse)
+      tree(last, ops.reverse)
+    }
+
+    val expr3: Parser[Expression] = (rep(expr2 ~ PLUS() | expr2 ~ MINUS()) ~ expr2) ^^ { case ops ~ last =>
+      def tree(last: Expression, ops: Seq[~[Expression, Token]]): Expression = ops match {
+        case op :: rest => op match {
+          case number ~ PLUS() => Add(tree(number, rest), last)
+          case number ~ MINUS() => Subtract(tree(number, rest), last)
+        }
+        case _ => last
+      }
+
+      tree(last, ops.reverse)
     }
 
     def parse(tokens: Seq[Token]): Either[CompileError, ArithmeticAst] = {
       val reader = new TokenReader(tokens)
-      plusMinus(reader) match {
+      expr3(reader) match {
         case NoSuccess(msg, next) => Left(CompileError(next.pos.column, msg))
         case Success(expression, next) => Right(new ArithmeticAst(expression))
       }
--- modules/apis/workouts/test/dsl/compile/ArithmeticCompilerSpec.scala
+++ modules/apis/workouts/test/dsl/compile/ArithmeticCompilerSpec.scala
@@ -26,6 +26,16 @@ class ArithmeticCompilerSpec
       expression mustBe Subtract(Add(Number(1), Number(2)), Number(3))
     }
 
+    "enforce correct order of operations for addition with multiplication" in {
+      val expression = ArithmeticCompiler.compile("1 + 2 * 3 + 4").right.get.expression
+      expression mustBe Add(Add(Number(1), Multiply(Number(2), Number(3))), Number(4))
+    }
+
+    "respect parantheses" in {
+      val expression = ArithmeticCompiler.compile("(1 + 2) * (3 + 4)").right.get.expression
+      expression mustBe Multiply(Add(Number(1), Number(2)), Add(Number(3), Number(4)))
+    }
+
   }
 
 }
